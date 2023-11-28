5a52f13bd2eca0ba188183bd8e619895b6eb8310
César Zárate <cesarjzo@outlook.com>
2023-11-28 01:15:50 -0600

    Add createProduct function (needs test)

--- src/stripe/stripe.js
+++ src/stripe/stripe.js
@@ -20,3 +20,23 @@ export const checkout = async (priceId) => {
     const body = await response.json()
     window.location.href = body.url
 }
+
+export const createProduct = async (courseName, description, price) => {
+    const data = {
+        courseName: courseName,
+        description: description,
+        price: price
+    }
+
+    const dataJson = JSON.stringify(data)
+
+    const response = await fetch(`${baseUrl}/create-product`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: dataJson,
+    })
+    
+    return response.status === 200
+}
